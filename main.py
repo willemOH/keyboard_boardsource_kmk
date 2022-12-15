bbfefa8956d746873c5dcd1d8878867ee2f74830
willemOH <willemhilliard@gmail.com>
2022-12-15 00:25:33 -0500

    symbol update and cleanup

--- main.py
+++ main.py
@@ -35,38 +35,38 @@ keyboard.keymap = [
      # |------+------+------+------+------+------|------+------+------+------+------+------|
      # | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /   |Enter |
      # |------+------+------+------+------+------+------+------+------+------+------+------|
-     # | Caps |L_Ctrl|  Alt |  GUI |NavLr |  cmd?|      |Sc/Sym| PadLr|PrntSc|Insert|  \   |
+     # | Caps |L_Ctrl|  Alt |  GUI |NavLr |  cmd?|      |Sc/Sym| PadLr|PrntSc|Insert| sys  |
      # `-----------------------------------------------------------------------------------'
     [  #MAIN
         KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.BSPC,
         KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT,
         KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.ENT ,
-        KC.CAPS, KC.LCTL, KC.LALT, KC.LGUI, NAV,     KC.SPC,  SYMBOL,  KC.SPC,  KC.LEFT, KC.PSCR, KC.INS,  KC.BSLASH
-    ],
-
+        KC.CAPS, KC.LCTL, KC.LALT, KC.LGUI, NAV,     KC.SPC,  SYMBOL,  KC.SPC,  KC.LEFT, KC.PSCR, KC.INS,  XXXXXXX
+    ],                              
+                                                                                            
      # Layer _NAV
      # ,-----------------------------------------------------------------------------------.
      # |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Undo | Cut  | Copy |Paste |  Del |
      # |------+------+------+------+------+-------------+------+------+------+------+------|
-     # |      | Home | PgDn | PgUp | End  |      |      | Left | Up   | Down | Right|      |
+     # |  Esc | Home | PgDn | PgUp | End  |      |      | Left | Up   | Down | Right|      |
      # |------+------+------+------+------+------|------+------+------+------+------+------|
      # |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |      |      |      |      |      |
      # |------+------+------+------+------+------+------+------+------+------+------+------|
-     # |      |      |      |      |LShift|MainLr| Shift| Space| Left |  Up  | Down | Right|      
+     # |      |      |      |      |LCtrl |MainLr| Shift| Space| Left |  Up  | Down | Right|      
      # `-----------------------------------------------------------------------------------'
-     
+           
     [  #NAV 
-        KC.GRV,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   UNDO,    CUT,     COPY,    PASTE,    KC.BSPC,
-        XXXXXXX, KC.HOME, KC.PGDN, KC.PGUP, KC.END,  XXXXXXX, XXXXXXX, KC.LEFT, KC.UP,   KC.DOWN, KC.RIGHT, XXXXXXX,
+        KC.GRV,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   UNDO,    CUT,     COPY,    PASTE,    KC.DEL,
+        KC.ESC, KC.HOME, KC.PGDN, KC.PGUP, KC.END,  XXXXXXX, XXXXXXX, KC.LEFT, KC.UP,   KC.DOWN, KC.RIGHT, XXXXXXX,
         XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.LCTL, MAIN,    KC.RSFT, KC.SPC,  KC.LEFT, KC.UP,   KC.DOWN,  KC.RIGHT
     ],
     
      # Layer _SYMBOL
      # ,-----------------------------------------------------------------------------------.
-     # |      |   !  |   @  |   #  |   $  |  %   |   ^  |   &  |   *  |   (  |   )  |      |
+     # |   ~  |   !  |   @  |   #  |   $  |  %   |   ^  |   &  |   *  |   (  |   )  |      |
      # |------+------+------+------+------+-------------+------+------+------+------+------|
-     # |      |   _  |   {  |   }  |   +  |      |      |   -  |   =  |   [  |   ]  |  \   |
+     # |      |   {  |   }  |   +  |   _  |      |      |   -  |   =  |   [  |   ]  |  \   |
      # |------+------+------+------+------+------|------+------+------+------+------+------|
      # |      |      |   7  |  8   |   9  |      |      |      |      |      |      |      |
      # |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -75,7 +75,7 @@ keyboard.keymap = [
      
     [  #SYMBOL
         KC.TILD, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,
-        KC.DEL,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR, KC.PIPE,
+        XXXXXXX, KC.LCBR,   KC.RCBR,   KC.PLUS,   KC.UNDS,   KC.F5,   KC.F6,   KC.MINS, KC.EQL, KC.LBRC, KC.RBRC, KC.BSLS,
         XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX, XXXXXXX, KC.HOME, KC.END,  XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.MNXT, KC.VOLD, KC.VOLU, KC.MPLY
     ],
