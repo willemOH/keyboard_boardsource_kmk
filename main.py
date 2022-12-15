310101bc06ba8489b81492e04f87d8eaa9d7d0b3
willemOH <willemhilliard@gmail.com>
2022-12-14 22:59:31 -0500

    symbol layer function change, pad layer

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ XXXXXXX = KC.NO
 
 MAIN = KC.TO(0) 
 NAV = KC.TT(1)
-SYMBOL = KC.LT(2, KC.SPC)
+SYMBOL = KC.LT(2, 3)
 
 UNDO = KC.LCTL(KC.Z)
 CUT = KC.LCTL(KC.X)
 COPY = KC.LCTL(KC.C)
-PASTE = KC.LCTL(KC.V)
+PASTE = KC.LCTL(KC.V)             
 
 keyboard.keymap = [
 
@@ -41,7 +41,7 @@ keyboard.keymap = [
         KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.BSPC,
         KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT,
         KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.ENT ,
-        KC.CAPS, KC.LCTL, KC.LALT, KC.LGUI, NAV,     KC.SPC,  KC.SPC,  SYMBOL,  KC.LEFT, KC.PSCR, KC.INS,  KC.BSLASH
+        KC.CAPS, KC.LCTL, KC.LALT, KC.LGUI, NAV,     KC.SPC,  SYMBOL,  KC.SPC,  KC.LEFT, KC.PSCR, KC.INS,  KC.BSLASH
     ],
 
      # Layer _NAV
@@ -52,16 +52,22 @@ keyboard.keymap = [
      # |------+------+------+------+------+------|------+------+------+------+------+------|
      # |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |      |      |      |      |      |
      # |------+------+------+------+------+------+------+------+------+------+------+------|
-     # |      |      |      |      |LShift|MainLr| Space| Shift| Left |  Up  | Down | Right|      
+     # |      |      |      |      |LShift|MainLr| Shift| Space| Left |  Up  | Down | Right|      
      # `-----------------------------------------------------------------------------------'
      
     [  #NAV 
-        KC.GRV,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   UNDO, CUT, COPY, PASTE, KC.BSPC,
-        XXXXXXX, KC.HOME, KC.PGDN, KC.PGUP, KC.END,  XXXXXXX, XXXXXXX, KC.LEFT,       KC.UP,         KC.DOWN,       KC.RIGHT,      XXXXXXX,
-        XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX,       XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC.LCTL, KC.LSFT, MAIN, KC.SPC, KC.RSFT, KC.LEFT, KC.UP, KC.DOWN, KC.RIGHT
+        KC.GRV,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   UNDO,    CUT,     COPY,    PASTE,    KC.BSPC,
+        XXXXXXX, KC.HOME, KC.PGDN, KC.PGUP, KC.END,  XXXXXXX, XXXXXXX, KC.LEFT, KC.UP,   KC.DOWN, KC.RIGHT, XXXXXXX,
+        XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC.LCTL, KC.LSFT, MAIN,    KC.RSFT, KC.SPC,  KC.LEFT, KC.UP,   KC.DOWN,  KC.RIGHT
     ],
     [  #SYMBOL
+        KC.TILD, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,
+        KC.DEL,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR, KC.PIPE,
+        XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX, XXXXXXX, KC.HOME, KC.END,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.MNXT, KC.VOLD, KC.VOLU, KC.MPLY
+    ],
+    [  #PAD
         KC.TILD, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,
         KC.DEL,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR, KC.PIPE,
         XXXXXXX, KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  XXXXXXX, XXXXXXX, KC.HOME, KC.END,  XXXXXXX,
