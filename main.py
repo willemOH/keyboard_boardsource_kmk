22d3013ca26ab917e555246e9f91a33c469960a7
willemOH <willemhiliard.com>
2023-01-17 22:12:14 -0500

    thumb power pt1
    main layer

--- main.py
+++ main.py
@@ -45,10 +45,10 @@ keyboard.keymap = [
      # | Caps |      |  Alt |  GUI |  NAV | Ctrl | Shift|Sp/SYM|      |PrntSc|Insert| SYS  |
      # `-----------------------------------------------------------------------------------' 
     [  #MAIN
-        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R, KC.T,   KC.Y, KC.U, KC.I,     KC.O,    KC.P,    KC.BSPC,
-        KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F, KC.G,   KC.H, KC.J, KC.K,     KC.L,    KC.SCLN, KC.QUOT,
-        PAD, KC.Z,    KC.X,    KC.C,    KC.V, KC.B,   KC.N, KC.M, KC.COMM,  KC.DOT,  KC.SLSH, KC.ENT ,
-        KC.CAPS, XXXXXXX, KC.LALT, KC.LGUI, NAV,  KC.LCTL, KC.RSFT, SYMBOL, XXXXXXX, KC.PSCR, KC.INS,  SYS
+        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R, KC.T,    KC.Y,   KC.U,   KC.I,     KC.O,    KC.P,    KC.BSPC,
+        KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F, KC.G,    KC.H,   KC.J,   KC.K,     KC.L,    KC.SCLN, KC.QUOT,
+        PAD,     KC.Z,    KC.X,    KC.C,    KC.V, KC.B,    KC.N,   KC.M,   KC.COMM,  KC.DOT,  KC.SLSH, KC.BSLS,      
+        SYS,     KC.CAPS, KC.LALT, KC.LCTL, NAV,  KC.LGUI, KC.ENT, SYMBOL, KC.LSFT,  XXXXXXX, KC.INS,  KC.PSCR 
     ],                              
                                                                                             
      # Layer _NAV
